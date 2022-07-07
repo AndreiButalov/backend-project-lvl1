db7fcf4a91156e89bde1f99653cf251fc77ba894
AndrejYaotsin <andrej.butalov@gmx.de>
2022-07-07 18:53:35 +0200

    calc

--- src/Games/brain-calc.js
+++ src/Games/brain-calc.js
@@ -6,11 +6,27 @@ const startCalc = () => {
     const name = readlineSync.question("May I have your name? ");
     console.log(`Hello, ${name}!`);
     console.log("What is the result of the expression?");
+	
+
 
     for(let i = 0; i < 3; i++) {
+	const operator = ['+', '-', '*'];
+	    
         const ranNum = randomNumer(1, 100);
-        console.log(`Question: `);
+	const num1 = ranNum();
+	const num2 = ranNum();
+	
+	const calculate = (num1, num2, operator) =>{
+		switch (operator) {
+		   default: return null;
+		   case ('+'): return num1 + num2;
+		   case ('-'): return num1 - num2;
+		   case ('*'): return num1 * num2;
+		}
+	};    
+        console.log(`Question: ${num1} ${operaror} ${num2}`);
         const userAnswer = readlineSync.question("Your answer: ");
+	const rightAnswer = calculate(num1, num2, operator);
     }    
     
 };
