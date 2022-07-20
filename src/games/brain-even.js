749ce21278ea6c4b5c8e94d3b851e37def8d3ee9
AndrejYaotsin <andrej.butalov@gmx.de>
2022-07-20 16:30:52 +0200

    gcd.js

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -7,8 +7,7 @@ const bedinung = ("Answer 'yes' if the number is even, otherwise 'no'.");
 function gamesRound() {
     const question = randomNumber(1, 100);
     const correctAnswer = question % 2 === 0 ? 'yes' : 'no';
-    return [question, correctAnswer];
-    
+    return [question, correctAnswer];    
 };
 
 function startEven() {
