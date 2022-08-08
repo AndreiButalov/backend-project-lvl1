b3f0bbf8dd679edc5f2a45e983f2a50eb9494918
AndrejYaotsin <andrej.butalov@gmx.de>
2022-08-08 14:04:00 +0200

    progression

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -4,8 +4,30 @@ import startGames from '../index.js';
 
 const task = ("What number is missing in the progression?");
 
+function ranProgression(firstIndex, schrit, ranLength) {
+    const arr = [];
+    for (let i = 0; i <= ranLength; i++) {
+        arr.push(firstIndex + schrit * i);
+    }
+    return arr;
+}
+
+function gamesRound() {
+    const firstIndex = randomNumber(1, 10);
+    const schrit = randomNumber(3, 20);
+    const ranLength = randomNumber(5, 10);
+
+    const progression = ranProgression(firstIndex, schrit, ranLength);
+    const index = randomNumber(0, progression.length - 1);
+
+    const correctAnswer = String(progression[index]);
+    progression[index] = '..';
+    const question = progression;
+    return [question, correctAnswer];
+} 
+
 function startProgression() {
-    
+    startGames(task, gamesRound);    
 };
 
 export default startProgression;
